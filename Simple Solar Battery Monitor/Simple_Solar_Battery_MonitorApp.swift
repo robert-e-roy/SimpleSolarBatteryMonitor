b41214d71f06f18478c327ae3e4c8637849251f0
Robert Roy <me@robert-e-roy.com>
2021-01-24 15:54:09 -0800

    Initial Commit

---
+++ Simple Solar Battery Monitor/Simple_Solar_Battery_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Solar_Battery_MonitorApp.swift
+//  Simple Solar Battery Monitor
+//
+//  Created by Robert Roy on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Solar_Battery_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
