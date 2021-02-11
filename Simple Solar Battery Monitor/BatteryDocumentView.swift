c9f8fd155e860c9bafdce93ae00324d714a74f7f
Robert Roy <me@robert-e-roy.com>
2021-02-11 15:26:08 -0800

    adjust battery percents and scrollview

--- Simple Solar Battery Monitor/BatteryDocumentView.swift
+++ Simple Solar Battery Monitor/BatteryDocumentView.swift
@@ -20,65 +20,92 @@ struct BatteryDocumentView: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                ScrollView(.vertical){
-                    HStack{
-                        let maxvolts = String(format: "%.2f", battery.maxVolts)
-                        Text ( " Voltage: \(maxvolts)")
-                    }
-                    ForEach (battery.logs ) { log in
-                        let voltage = String(format: "%.2f", log.volts)
-                        let date = LogDate(date: log.date)
-                        let color = percent(current: log.volts ,max: battery.maxVolts)
-                        
-                        HStack {
-                            Text( "\(voltage)" )
-                            Text( " On: \(date)" )
-                           // Text( " Percent : \(percent)" )
-                        }.background(color)
-                        .onLongPressGesture {
-                            if self.editMode.isEditing {
-                                battery.removeVoltageLog(log: log)
-                            }
+        Group {
+            HStack{
+                let maxvolts = String(format: "%.2f", battery.maxVolts)
+                Text ( "Max Voltage: \(maxvolts)")
+            }
+            
+            ScrollView(.vertical){
+                
+                ForEach (battery.logs ) { log in
+                    let voltage = String(format: "%.2f", log.volts)
+                    let date = LogDate(date: log.date)
+                    let (color,percent) = percent(current: log.volts ,max: battery.maxVolts)
+                    let percentString = String(format: "%.0f", percent)
+                    HStack {
+                        Text( "\(voltage)" )
+                        Text( " On: \(date)" )
+                        Text( " \(percentString) %" )
+                    }.background(color)
+                    .onLongPressGesture {
+                        if self.editMode.isEditing {
+                            battery.removeVoltageLog(log: log)
                         }
                     }
-                    TextField("new measurement" , text: $addLogValue)
-                            { isEditing in
-                                self.isEditing = isEditing
-                            } onCommit: {
-                                if let new = Float( addLogValue) {
-                                        battery.addVoltageLog(volts: new)
-                                }
-                                addLogValue = ""
-                            }
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
+                TextField("new measurement" , text: $addLogValue)
+                { isEditing in
+                    self.isEditing = isEditing
+                } onCommit: {
+                    if let new = Float( addLogValue) {
+                        battery.addVoltageLog(volts: new)
+                    }
+                    addLogValue = ""
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                //.keyboardType(.decimalPad) no done button
             }
-        
         }
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        .font(.body)
         .navigationBarItems(
             trailing: EditButton()
         )
         .environment(\.editMode, $editMode)
-
+        
     }
 }
+
 func LogDate ( date:Date) -> String {
     let df = DateFormatter()
     df.setLocalizedDateFormatFromTemplate("MMMdd HH:mm")
     return df.string(from: date)
     }
-
-func percent ( current: Float, max: Float) -> Color {
+enum cellVoltage: Float {
+    case hundred = 12.6
+    case ninty = 12.5
+    case eighty = 12.42
+    case seventy = 12.32
+    case sixty = 12.20
+    case fifty = 12.06
+    case foury = 11.90
+    case thrity = 11.75
+    case twenty = 11.58
+    case ten = 11.31
+}
+func percent ( current: Float, max: Float) -> (Color,Float) {
     
-    let percent = (current - 12) / (max - 12) * 100
-    print ("percent \(percent)")
-    if percent > 60 {
-        return Color.green
-    }
-    else{
-        return Color.red
+    if current >= cellVoltage.hundred.rawValue {
+        return (Color.green,100)
+    } else if ( current >= cellVoltage.ninty.rawValue ) {
+        return (Color.green,90)
+    } else if ( current >= cellVoltage.eighty.rawValue ) {
+        return (Color.green,80)
+    } else if ( current >= cellVoltage.seventy.rawValue ) {
+        return (Color.green,70)
+    } else if ( current >= cellVoltage.sixty.rawValue ) {
+        return (Color.green,60)
+    } else if ( current >= cellVoltage.fifty.rawValue ) {
+        return (Color.green,50)
+    } else if ( current >= cellVoltage.foury.rawValue ) {
+        return (Color.green,40)
+    } else if ( current >= cellVoltage.thrity.rawValue ) {
+        return (Color.yellow,30)
+    } else if ( current >= cellVoltage.twenty.rawValue ) {
+        return (Color.yellow,20)
+    } else if ( current >= cellVoltage.ten.rawValue ) {
+        return (Color.red,10)
+    } else {
+        return (Color.red,0)
     }
 }
